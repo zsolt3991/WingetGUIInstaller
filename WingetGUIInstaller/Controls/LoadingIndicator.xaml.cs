d5b33b34911cd3f965528eff12851f5db1435bad
Zsolt Szilagyi <zsolt_93@yahoo.com>
2022-04-10 19:48:59 +0300

    Separate Loading Indicator as a Custom Control

---
+++ WingetGUIInstaller/Controls/LoadingIndicator.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+
+namespace WingetGUIInstaller.Controls
+{
+    public sealed partial class LoadingIndicator : UserControl
+    {
+        public static readonly DependencyProperty MessageProperty = DependencyProperty
+            .Register("Message", typeof(string), typeof(LoadingIndicator), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty IsLoadingProperty = DependencyProperty
+            .Register("IsLoading", typeof(bool), typeof(LoadingIndicator), new PropertyMetadata(false));
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public bool IsLoading
+        {
+            get { return (bool)GetValue(IsLoadingProperty); }
+            set { SetValue(IsLoadingProperty, value); }
+        }
+
+        public LoadingIndicator()
+        {
+            InitializeComponent();
+        }
+    }
+}
