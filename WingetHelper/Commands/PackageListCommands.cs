b84989ea368b8f82641f0901c3ce9382518354a1
Zsolt Szilagyi <zsolt_93@yahoo.com>
2022-05-12 22:18:08 +0300

    #24 Create Command structure for Import Export Wrapper

---
+++ WingetHelper/Commands/PackageListCommands.cs
@@ -0,0 +1,58 @@
+﻿namespace WingetHelper.Commands
+{
+    public static class PackageListCommands
+    {
+        public static WingetCommand<object> ExportPackagesToFile(string filePath, bool includeVersions = false,
+            bool acceptSourceAgreements = false, string packageSourceFilter = default)
+        {
+            var command = new WingetCommand<object>("export", "-o", filePath)
+                .ConfigureResultDecoder(commandResult => null);
+
+            if (includeVersions)
+            {
+                command.AddExtraArguments("--include-versions");
+            }
+
+            if (acceptSourceAgreements)
+            {
+                command.AddExtraArguments("--accept-source-agreements");
+            }
+
+            if (packageSourceFilter != default)
+            {
+                command.AddExtraArguments("-s", packageSourceFilter);
+            }
+
+            return command;
+        }
+
+        public static WingetCommand<object> ImportPackagesFromFile(string filePath, bool ignoreUnavailable = true,
+            bool ignoreVersions = false, bool acceptPackageAgreements = false, bool acceptSourceAgreements = false)
+        {
+            var command = new WingetCommand<object>("import", "-i", filePath)
+                 .ConfigureResultDecoder(commandResult => null);
+
+            if (ignoreUnavailable)
+            {
+                command.AddExtraArguments("--ignore-unavailable");
+            }
+
+            if (ignoreVersions)
+            {
+                command.AddExtraArguments("--ignore-versions");
+            }
+
+            if (acceptPackageAgreements)
+            {
+                command.AddExtraArguments("--accept-package-agreements");
+            }
+
+            if (acceptSourceAgreements)
+            {
+                command.AddExtraArguments("--accept-source-agreements");
+            }
+
+            return command;
+        }
+    }
+}
---
+++ WingetHelper/Models/WingetPackageListExport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WingetHelper.Models
+{
+    internal class WingetPackageListExportData
+    {
+        public DateTime CreationDate { get; set; }
+        public PackageGroupExportData[] Sources { get; set; }
+        public string WinGetVersion { get; set; }
+    }
+
+    public class PackageGroupExportData
+    {
+        public PackageExportData[] Packages { get; set; }
+        public PackageSourceExportData SourceDetails { get; set; }
+    }
+
+    public class PackageSourceExportData
+    {
+        public string Argument { get; set; }
+        public string Identifier { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+
+    public class PackageExportData
+    {
+        public string PackageIdentifier { get; set; }
+        public string Version { get; set; }
+        public string Channel { get; set; }
+        public string Scope { get; set; }
+    }
+}
+
