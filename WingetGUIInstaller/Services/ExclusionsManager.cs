3aecb264be685b80cd950508ec0bf173004705b8
Zsolt Szilagyi <zsolt_93@yahoo.com>
2022-11-06 14:54:56 +0200

    Move Package source exclusions to ExclusionsManager

--- WingetGUIInstaller/Services/ExclusionsManager.cs
+++ WingetGUIInstaller/Services/ExclusionsManager.cs
@@ -9,6 +9,7 @@ namespace WingetGUIInstaller.Services
     {
         private readonly ApplicationDataStorageHelper _configurationStore;
         private List<string> _excludedPackageIds;
+        private List<string> _excludedPackageSources;
 
         public ExclusionsManager(ApplicationDataStorageHelper configurationStore)
         {
@@ -18,14 +19,8 @@ namespace WingetGUIInstaller.Services
         public bool ExcludedPackagesEnabled => _configurationStore
             .Read(ConfigurationPropertyKeys.ExcludedPackagesEnabled, ConfigurationPropertyKeys.ExcludedPackagesEnabledDefaultValue);
 
-        public List<string> GetExclusions()
-        {
-            if (_excludedPackageIds == default)
-            {
-                _excludedPackageIds = LoadPackageExclusionList();
-            }
-            return _excludedPackageIds;
-        }
+        public bool ExcludedPackageSourcesEnabled => _configurationStore
+            .Read(ConfigurationPropertyKeys.PackageSourceFilteringEnabled, ConfigurationPropertyKeys.PackageSourceFilteringEnabledDefaultValue);
 
         public bool AddPackageExclusion(string exclusionId)
         {
@@ -70,6 +65,49 @@ namespace WingetGUIInstaller.Services
             return false;
         }
 
+        public bool AddPackageSourceExclusion(string packageSourceName)
+        {
+            if (_excludedPackageSources == default)
+            {
+                _excludedPackageSources = LoadPackageSourceExclusionList();
+            }
+            if (!_excludedPackageSources.Contains(packageSourceName))
+            {
+                _excludedPackageSources.Add(packageSourceName);
+                SavePackageSourceExclusionList(_excludedPackageSources);
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemovePackageSourceExclusion(string packageSourceName)
+        {
+            if (_excludedPackageSources == default)
+            {
+                _excludedPackageSources = LoadPackageSourceExclusionList();
+            }
+            if (_excludedPackageSources.Contains(packageSourceName))
+            {
+                _excludedPackageSources.Remove(packageSourceName);
+                SavePackageSourceExclusionList(_excludedPackageIds);
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsPackageSourceExcluded(string packageSourceName, bool overrideFeatureEnabled = false)
+        {
+            if (_excludedPackageSources == default)
+            {
+                _excludedPackageSources = LoadPackageSourceExclusionList();
+            }
+            if (_excludedPackageSources.Contains(packageSourceName))
+            {
+                return overrideFeatureEnabled || ExcludedPackageSourcesEnabled;
+            }
+            return false;
+        }
+
         private List<string> LoadPackageExclusionList()
         {
             var storedIds = _configurationStore.Read(ConfigurationPropertyKeys.ExcludedPackageIds,
@@ -87,6 +125,25 @@ namespace WingetGUIInstaller.Services
         {
             var idsToStore = JsonSerializer.Serialize(exclusionList);
             _configurationStore.Save(ConfigurationPropertyKeys.ExcludedPackageIds, idsToStore);
+        }
+
+        private List<string> LoadPackageSourceExclusionList()
+        {
+            var storedSources = _configurationStore.Read(ConfigurationPropertyKeys.DisabledPackageSources,
+                ConfigurationPropertyKeys.DisabledPackageSourcesDefaultValue);
+
+            if (string.IsNullOrEmpty(storedSources))
+            {
+                return new List<string>();
+            }
+
+            return JsonSerializer.Deserialize<List<string>>(storedSources);
+        }
+
+        private void SavePackageSourceExclusionList(List<string> packageSources)
+        {
+            var sourcesToStore = JsonSerializer.Serialize(packageSources);
+            _configurationStore.Save(ConfigurationPropertyKeys.DisabledPackageSources, sourcesToStore);
         }
     }
 }
