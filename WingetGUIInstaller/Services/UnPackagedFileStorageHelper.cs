4ace6f47278f50714f1bf4f926a99f72f767295d
Zsolt Szilagyi <zsolt_93@yahoo.com>
2023-06-17 15:10:42 +0300

    Create Unpackaged Implementation for the Packaged Helpers

---
+++ WingetGUIInstaller/Services/UnPackagedFileStorageHelper.cs
@@ -0,0 +1,162 @@
+﻿using CommunityToolkit.Common.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WingetGUIInstaller.Services
+{
+    internal sealed class UnPackagedFileStorageHelper : IFileStorageHelper
+    {
+        private const string ApplicationFolderName = "WingetGuiInstaller";
+        private readonly string _basePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+        public async Task CreateFileAsync<T>(string filePath, T value)
+        {
+            var completePath = Path.Combine(_basePath, ApplicationFolderName, filePath);
+            ValidatePath(completePath);
+
+            var fileContent = JsonSerializer.Serialize<T>(value);
+            await File.WriteAllTextAsync(completePath, fileContent);
+        }
+
+        public Task CreateFolderAsync(string folderPath)
+        {
+            var completePath = Path.Combine(_basePath, ApplicationFolderName, folderPath);
+            ValidatePath(completePath);
+
+            Directory.CreateDirectory(completePath);
+            return Task.CompletedTask;
+        }
+
+        public async Task<T> ReadFileAsync<T>(string filePath, T defaultValue = default)
+        {
+            var completePath = Path.Combine(_basePath, ApplicationFolderName, filePath);
+            ValidatePath(completePath);
+
+            if (!File.Exists(completePath))
+            {
+                return defaultValue;
+            }
+
+            try
+            {
+                using (var fileStream = File.Open(completePath, FileMode.Open))
+                {
+                    return await JsonSerializer.DeserializeAsync<T>(fileStream);
+                }
+            }
+            catch
+            {
+                return defaultValue;
+            }
+        }
+
+        public Task<IEnumerable<(DirectoryItemType ItemType, string Name)>> ReadFolderAsync(string folderPath)
+        {
+            var completePath = Path.Combine(_basePath, ApplicationFolderName, folderPath);
+            ValidatePath(completePath);
+
+            var resultSet = new List<(DirectoryItemType, string)>();
+
+            if (!Directory.Exists(completePath))
+            {
+                return Task.FromResult<IEnumerable<(DirectoryItemType ItemType, string Name)>>(resultSet);
+            }
+
+            foreach (var subDirectory in Directory.GetDirectories(completePath))
+            {
+                resultSet.Add(new(DirectoryItemType.Folder, subDirectory));
+            }
+
+            foreach (var file in Directory.GetFiles(completePath))
+            {
+                resultSet.Add(new(DirectoryItemType.File, file));
+            }
+
+            return Task.FromResult<IEnumerable<(DirectoryItemType ItemType, string Name)>>(resultSet);
+        }
+
+        public Task<bool> TryDeleteItemAsync(string itemPath)
+        {
+            var completePath = Path.Combine(_basePath, ApplicationFolderName, itemPath);
+            ValidatePath(completePath);
+
+            if (Directory.Exists(completePath))
+            {
+                try
+                {
+                    Directory.Delete(completePath);
+                    return Task.FromResult(true);
+                }
+                catch
+                {
+                    return Task.FromResult(false);
+                }
+            }
+
+            if (Directory.Exists(completePath))
+            {
+                try
+                {
+                    File.Delete(completePath);
+                    return Task.FromResult(true);
+                }
+                catch
+                {
+                    return Task.FromResult(false);
+                }
+            }
+
+            return Task.FromResult(false);
+        }
+
+        public Task<bool> TryRenameItemAsync(string itemPath, string newName)
+        {
+            var oldPath = Path.Combine(_basePath, ApplicationFolderName, itemPath);
+            var newPath = Path.Combine(_basePath, ApplicationFolderName, newName);
+            ValidatePath(oldPath);
+            ValidatePath(newPath);
+
+            if (Directory.Exists(oldPath))
+            {
+                try
+                {
+                    Directory.Move(oldPath, newPath);
+                    return Task.FromResult(true);
+                }
+                catch
+                {
+                    return Task.FromResult(false);
+                }
+            }
+
+            if (Directory.Exists(oldPath))
+            {
+                try
+                {
+                    File.Move(oldPath, newPath);
+                    return Task.FromResult(true);
+                }
+                catch
+                {
+                    return Task.FromResult(false);
+                }
+            }
+
+            return Task.FromResult(false);
+        }
+
+        private void ValidatePath(string path)
+        {
+            var basePath = Path.Combine(_basePath, ApplicationFolderName);
+            var completePath = Path.GetFullPath(path);
+
+            if (!completePath.StartsWith(basePath))
+            {
+                throw new InvalidOperationException("Accessing a path outside the application directory is forbidden");
+            }
+        }
+    }
+}
---
+++ WingetGUIInstaller/Services/UnpackagedSettingStorageHelper.cs
@@ -0,0 +1,88 @@
+﻿using CommunityToolkit.Common.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WingetGUIInstaller.Services
+{
+    internal sealed class UnpackagedSettingStorageHelper : ISettingsStorageHelper<string>
+    {
+        private const string SettingsFileName = "settings.json";
+        private readonly IFileStorageHelper _fileStorage;
+        private readonly SemaphoreSlim _fileAccessSemaphore;
+        private IDictionary<string, string> _settings;
+
+        public UnpackagedSettingStorageHelper(IFileStorageHelper fileStorage)
+        {
+            _fileStorage = fileStorage;
+            _settings = _fileStorage.ReadFileAsync(SettingsFileName, new Dictionary<string, string>()).GetAwaiter().GetResult();
+            _fileAccessSemaphore = new SemaphoreSlim(1);
+        }
+
+        public void Clear()
+        {
+            if (!_fileStorage.TryDeleteItemAsync(SettingsFileName).GetAwaiter().GetResult())
+            {
+                throw new InvalidOperationException("Failed to clear application settings");
+            };
+        }
+
+        public void Save<TValue>(string key, TValue value)
+        {
+            _settings.TryAdd(key, JsonSerializer.Serialize(value));
+            SyncSettingsFile();
+        }
+
+        public bool TryDelete(string key)
+        {
+            if (!_settings.ContainsKey(key))
+            {
+                return false;
+            }
+            _settings.Remove(key);
+            SyncSettingsFile();
+            return true;
+        }
+
+        public bool TryRead<TValue>(string key, out TValue value)
+        {
+            if (!_settings.TryGetValue(key, out string serializedValue))
+            {
+                value = default;
+                return false;
+            };
+
+            value = JsonSerializer.Deserialize<TValue>(serializedValue);
+            return true;
+        }
+
+        private void SyncSettingsFile()
+        {
+            Exception syncException = default;
+            ManualResetEvent writeCompleteEvent = new ManualResetEvent(false);
+            _fileAccessSemaphore.Wait();
+
+            Task.Run(async () =>
+            {
+                try
+                {
+                    await _fileStorage.CreateFileAsync(SettingsFileName, _settings);
+                    writeCompleteEvent.Set();
+                }
+                catch (Exception innerException)
+                {
+                    syncException = innerException;
+                }
+            });
+
+            writeCompleteEvent.WaitOne();
+            _fileAccessSemaphore.Release();
+            if (syncException != default)
+            {
+                throw syncException;
+            }
+        }
+    }
+}
