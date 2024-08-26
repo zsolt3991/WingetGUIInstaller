8db2a9db9df17cd6fad029187d7d832ed05b06a2
Zsolt Szilagyi <zsolt_93@yahoo.com>
2024-08-26 11:04:44 +0300

    Cleanup the code in PackageUpdater project
    Fix Sonar and Microsoft Code Analysis messages

---
+++ GithubPackageUpdater/Models/PackageUpdateException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GithubPackageUpdater.Models
+{
+    public class PackageUpdateException : Exception
+    {
+        public PackageUpdateException(string message) : base(message)
+        {
+        }
+
+        public PackageUpdateException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
